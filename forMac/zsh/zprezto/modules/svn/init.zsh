d26120e570d511ad7bf5ce15379684eb15a3b1c8
unagoccia <una.goccia.della.luna@gmail.com>
2017-01-24 15:16:23 +0900

    svnの全コミット関数を作成

--- forMac/zsh/zprezto/modules/svn/init.zsh
+++ forMac/zsh/zprezto/modules/svn/init.zsh
@@ -9,3 +9,14 @@ fi
 
 alias svnlog='svn log --limit 10 -v'
 alias svndiffg='svn diff --diff-cmd fmdiff'
+alias svnaddall='(){
+    extention=$2
+    svn st $1 | awk -v "ext=$extention" '\'' match($1, "?") && match($2, ext) { print $2 } '\''
+    
+    echo -n "\ncontinue?(y/n):"
+    read isContinue
+    
+    if test ${isContinue} = "y"; then
+        svn st $1 | awk -v "ext=$extention" '\'' match($1, "?") && match($2, ext) { print $2 } '\'' | xargs svn add
+    fi
+}'
